ac488fcf74920a7f7e4b8a17e1cb195e14b416b9
Exanlv <exan@landviz.nl>
2025-08-29 16:58:08 +0200

    Replace spaces

--- src/Services/FileNamingService.php
+++ src/Services/FileNamingService.php
@@ -10,6 +10,8 @@ class FileNamingService
     {
         [$filename] = explode('.', $filename);
 
+        $filename = str_replace(' ', '_', $filename);
+
         return base64_encode(Carbon::now()->toIso8601String())
             . '.' . $filename . '.pdf';
     }
